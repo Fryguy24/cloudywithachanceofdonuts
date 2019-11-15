8dd99287ee6e0c0ba7aa60c312c6855b00d6f6a5
Julius Fry <fryjulius@gmail.com>
2019-11-15 11:06:47 -0600

    adds routes

--- server.js
+++ server.js
@@ -8,5 +8,29 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
   })
 
+// donut route
+
+// CREATE
+// ADD a new donut
+app.post('/donuts', (req, res) => {
+    // get an object via req. body
+    res.send('placeholder')
+})
+// GET donuts
+app.get('/donuts', (req, res) => {
+    res.send('sorry, no donuts')
+})
+// GET a single donut
+app.get('/donuts/:id', (req, res) => {
+    res.send('a single donut')
+})
+// DELETE donuts
+app.delete('/donuts/:id', (req, res) => {
+    res.send('deleted a donut')
+})
+// UPDATE donuts
+app.patch('/donuts/:id', (req, res) => {
+    res.send('updated a donut')
+})
 // listen on port
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
